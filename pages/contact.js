9c63bf827c9b4d9ca6f078c43ea9d561473d4e12
Louise Moxhay <louisemoxy@gmail.com>
2025-08-07 20:40:10 +0100

    Fix Form build issue

--- pages/contact.js
+++ pages/contact.js
@@ -17,7 +17,7 @@ export default function Contact({ globalData }) {
     const formData = new FormData(myForm);
 
     try {
-      await fetch("/", {
+      await fetch("/__forms.html", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: new URLSearchParams(formData).toString()
