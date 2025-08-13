2c3903b8d3fc7badce1ecfe95eb9672544091925
Louise Moxhay <louisemoxy@gmail.com>
2025-08-13 18:29:46 +0100

    add missing img

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,7 @@ import Layout, { GradientBackground } from '../components/Layout';
 import SEO from '../components/SEO';
 import Link from 'next/link';
 import { getGlobalData } from '../utils/global-data';
+import Image from 'next/image';
 
 export default function About({ globalData }) {
   return (
@@ -49,10 +50,7 @@ export default function About({ globalData }) {
             <div className="aspect-square rounded-2xl overflow-hidden shadow-2xl transform rotate-3 hover:rotate-0 transition-transform duration-300">
               <div className="w-full h-full bg-gradient-to-br from-gunmetal via-dark-slate-gray to-light-sea-green flex items-center justify-center">
                 <div className="text-center text-white p-8">
-                  <svg className="w-16 h-16 mx-auto mb-4 opacity-80" fill="currentColor" viewBox="0 0 20 20">
-                    <path fillRule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clipRule="evenodd" />
-                  </svg>
-                  <p className="text-sm opacity-90">Add your band image here</p>
+                  <Image src="/images/Denise.jpg" alt="Band in Performance" fill={true} className="object-cover" />
                 </div>
               </div>
             </div>
